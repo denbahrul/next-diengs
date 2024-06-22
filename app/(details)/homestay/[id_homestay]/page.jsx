37667b5ccbe76ffbe3ce32f26ee2cccae0f8d44f
maulanakhasan92 <you@example.com>
2024-06-22 22:33:49 +0700

    fix error typing in detail page

--- app/(details)/homestay/[id_homestay]/page.jsx
+++ app/(details)/homestay/[id_homestay]/page.jsx
@@ -1,19 +1,19 @@
-"use client"
-import { useRouter } from "next/router"
-import Description from "@/app/ui/detail/description"
-import Destination from "@/app/ui/detail/destination"
-import Facility from "@/app/ui/detail/Facility"
-import HotelImageHeader from "@/app/ui/detail/hotel-image-header"
-import ImageCard from "@/app/ui/detail/image-card"
-import Location from "@/app/ui/detail/location"
-import Navigation from "@/app/ui/detail/top-navigation/navigation"
-import ReviewList from "@/app/ui/detail/review/review-list"
-import RoomList from "@/app/ui/detail/room/room-list"
-import Rules from "@/app/ui/detail/rules"
-import Share from "@/app/ui/detail/top-navigation/share"
-import SelectRoomNav from "@/app/ui/detail/select-room-nav"
+"use client";
+import { useRouter } from "next/router";
+import Description from "@/app/ui/detail/description";
+import Destination from "@/app/ui/detail/destination";
+import Facility from "@/app/ui/detail/facility";
+import HotelImageHeader from "@/app/ui/detail/hotel-image-header";
+import ImageCard from "@/app/ui/detail/image-card";
+import Location from "@/app/ui/detail/location";
+import Navigation from "@/app/ui/detail/top-navigation/navigation";
+import ReviewList from "@/app/ui/detail/review/review-list";
+import RoomList from "@/app/ui/detail/room/room-list";
+import Rules from "@/app/ui/detail/rules";
+import Share from "@/app/ui/detail/top-navigation/share";
+import SelectRoomNav from "@/app/ui/detail/select-room-nav";
 
-export default function Page({ params: { id_homestay} }) {
+export default function Page({ params: { id_homestay } }) {
   return (
     <div>
       <div className="contine mx-auto lg:max-w-6xl">
