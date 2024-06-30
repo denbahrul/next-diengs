03f3589fb987b51c4324e6ad288bc40512b011f3
denbahrul <ulum0326@gmail.com>
2024-06-30 14:33:57 +0700

    adding e-ticket

--- app/(details)/ticket/page.jsx
+++ app/(details)/ticket/page.jsx
@@ -1,7 +1,17 @@
+import TopNav from "@/app/ui/top-nav"
+import QRCode from "@/app/ui/ticket/qr-code"
+import TicketDetail from "@/app/ui/ticket/ticket-detail"
+import ButtonBottom from "@/app/ui/button-bottom"
+
 export default function Ticket() {
     return (
-        <div>
-            <p>Ticket</p>
+        <div className="relative max-w-[420px] h-screen mx-auto bg-putih">
+            <TopNav title="Ticket" link="./homestay" />
+            <div className="mx-6 my-4 border-[1px] border-biru3 rounded-2xl">
+                <QRCode/>
+                <TicketDetail />
+            </div>
+            <ButtonBottom name="Download Tiket" />
         </div>
     )
 }
---
+++ app/ui/ticket/qr-code.jsx
@@ -0,0 +1,8 @@
+export default function QRCode() {
+    return (
+        <div className="p-10 border-b-2 border-biru3 border-dashed">
+            <p className="text-center font-bold">Homestay Langit Senja</p>
+            <img  src="QR-code.svg" alt="QR Code" className="m-auto"/>
+        </div>
+    )
+}
